5421ade52b9c20ec1f506f1f0d3cbe8a25bdde34
Ishmam Newaz <ishu.newaz@gmail.com>
2024-06-15 17:25:30 +0600

    version 5 (Added Face Recog)

--- openCam.py
+++ openCam.py
@@ -10,7 +10,19 @@ def generate_frames():
         success, frame = camera.read() #Returns 2 parameters 1 boolean 2 the frames
         if not success:
             break
-        else: 
+        else:
+            detector = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
+            eyes_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_eye.xml')
+            faces = detector.detectMultiScale(frame,1.1,7)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY)
+            #draw rectangles
+            for (x,y,w,h) in faces:
+                cv2.rectangle(frame,(x,y),(x+w, y+w),(255,0,0), 2)
+                roi_gray= gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eyes_cascade.detectMultiScale(roi_gray,1.1,3)
+                for(ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0),2)
             # takes the frames and encodes to jpg or png and returns 2 things Ret, buffer
             ret, buffer = cv2.imencode('.jpg',frame)
             frame = buffer.tobytes()
