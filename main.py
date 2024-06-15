257e43709ced0c4d831be58aa80854def0fb162f
Ishmam Newaz <ishu.newaz@gmail.com>
2024-06-15 15:56:09 +0600

    Version 2

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from flask import Flask,redirect,url_for,render_template,request
 app=Flask(__name__) #Name of the Flask Application Name
 @app.route('/') # This will trigger the next below function with same name. This is an app decorator for building URL
+
+
+# {%......%} conditions, for loops
+# {{}} expressions to print output
+# {#.....#} comments
+
+
+
+
 def welcome():
     return render_template("form.html")
 
 
-@app.route('/fail/<float:score>')
-def fail(score):
-    return render_template("failed.html", score = score)
+# @app.route('/fail/<float:score>')
+# def fail(score):
+#     return render_template("failed.html", score = score)
 
 
-@app.route('/passed/<float:score>')
-def passed(score):
-    return render_template("passed.html", score = score)
+# @app.route('/passed/<float:score>')
+# def passed(score):
+#     return render_template("passed.html", score = score)
 
-
+@app.route('/checker/<float:score>')
+def checker(score):
+    return render_template("result.html", score = score)
 
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
@@ -24,14 +35,8 @@ def submit():
         ban=float(request.form['bangla'])
         math=float(request.form['math'])
         ttl_score = (eng+ban+math)/3
-    
-    result=""
-    if ttl_score>49:
-        result = "passed"
-    else:
-        result = "fail"
 
-    return redirect(url_for(result, score=ttl_score))
+    return redirect(url_for('checker', score=ttl_score))
 
 
 if __name__ == '__main__': # Starting point of Program Application
